6fa57a07b9986525b6d2f39b3b465f2442f8332d
oluSammy <olumorinsammy@gmail.com>
2020-05-05 16:57:13 -0700

    created player data file and components folder

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+const players = [
+    {name: 'aguero', club: 'Man City', country: 'Argentina'},
+    {name: 'aubamayang', club: 'Arsenal', country: 'Gabon'},
+    {name: 'benzema', club: 'Real Madrid', country: 'France'},
+    {name: 'coutinho', club: 'Bayern Munchen', country: 'Brazil'},
+    {name: 'de-brunyne', club: 'Man City', country: 'Belgium'},
+    {name: 'dybala', club: 'Juventus', country: 'Argentina'},
+    {name: 'kante', club: 'Chelsea', country: 'France'},
+    {name: 'lacazette', club: 'Arsenal', country: 'France'},
+    {name: 'mane', club: 'Liverpool', country: 'Senegal'},
+    {name: 'martial', club: 'Man United', country: 'France'},
+    {name: 'mbappe', club: 'PSG', country: 'France'},
+    {name: 'messi', club: 'Barcelona', country: 'Argentina'},
+    {name: 'neuer', club: 'Bayern Munchen', country: 'Germany'},
+    {name: 'neymar', club: 'PSG', country: 'Brazil'},
+    {name: 'rashford', club: 'Man United', country: 'England'},
+    {name: 'ronaldo', club: 'Juventus', country: 'Portugal'},
+    {name: 'salah', club: 'Liverpool', country: 'Egypt'},
+    {name: 'sergio-ramos', club: 'Real Madrid', country: 'Spain'},
+    {name: 'suarez', club: 'Barcelona', country: 'Uruguay'},
+    {name: 'T Abraham', club: 'Chelsea', country: 'England'},
+];
+
+export default players;
